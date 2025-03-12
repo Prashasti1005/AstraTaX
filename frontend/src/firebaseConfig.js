35dcf5dd1b29c5644bb51ae7711e690d61631cee
Prashasti Singh <prashasti1005@gmail.com>
2025-03-12 21:46:24 +0530

    minor enhancements

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -2,16 +2,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
+// Ensure these environment variables are properly set in a .env file
 const firebaseConfig = {
-  apiKey: "AIzaSyDGV-v9JJAW4J4jqLnltX5LMAwDsIwReOQ",
-  authDomain: "astratax-d0104.firebaseapp.com",
-  projectId: "astratax-d0104",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+
 export const API_BASE_URL = "http://127.0.0.1:8000";
-
-
 export { auth, provider, signInWithPopup };
